25cf12c874736ab83401f7056c9617b46f82fdec
venky1240 <96647601+venky1240@users.noreply.github.com>
2022-01-12 06:32:22 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,21 @@
+// Compiling this Java program would
+// result in multiple class files.
+
+class Sample
+{
+
+}
+
+// Class Declaration
+class Student
+{
+
+}
+// Class Declaration
+class Test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Class File Structure");
+	}
+}
